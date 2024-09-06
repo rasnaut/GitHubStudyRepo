17d8a88f52cf5dff05477c060cbed84a02f9abcc
Natacool <natacool812@gmail.com>
2024-09-06 14:30:15 +0200

    [JS] Add change log from NataKul [Java] Add comment to Main.java [Java] Add HelloWorpd example on Java

---
+++ examples/Java/HelloWorld/Main.java
@@ -0,0 +1,7 @@
+package de.telran;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 console.log("Hello for Ivan");
 console.log("I can't understand how I did it");
+console.log("Log from Natalia Kulikova");
