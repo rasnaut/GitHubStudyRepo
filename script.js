b68425cbb0d493f1aad0ed35535249d29a8e1d08
Masha <165020790+MariiaShi@users.noreply.github.com>
2024-09-07 09:45:27 +0200

    Hola (#73)
    Co-authored-by: Maria <shirokova.maria14@gmail.comgit statusgit commitcd GitHubStudyRepo/git commitvigit config user.email shirokova.maria14@gmail.c>

--- script.js
+++ script.js
@@ -4,3 +4,5 @@ console.log("Hello for Ivan");
 console.log("I can't understand how I did it");
 console.log("New message from Ivan 060924");
 console.log ('message");
+console.log("Hola. Soy profesora de aleman");
+
