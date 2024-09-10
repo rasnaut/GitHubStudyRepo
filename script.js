415b67631d878cb6b5848dd26ccf319912ef79fa
Taisiia <165020409+Taisisia@users.noreply.github.com>
2024-09-10 09:59:47 +0100

    Revert "Hola (#73)"
    This reverts commit b68425cbb0d493f1aad0ed35535249d29a8e1d08.

--- script.js
+++ script.js
@@ -4,5 +4,3 @@ console.log("Hello for Ivan");
 console.log("I can't understand how I did it");
 console.log("New message from Ivan 060924");
 console.log ('message");
-console.log("Hola. Soy profesora de aleman");
-
